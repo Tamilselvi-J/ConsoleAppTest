8429e49ce0824cee508cc8dd7fcc652c4f0ef3ff
Tamilselvi-J <thaanu2000@gmail.com>
2025-07-07 16:01:53 +0530

    add optclass

---
+++ ConsoleAppGithub/Operations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppGithub
+{
+    internal class Operations
+    {
+        public void Run()
+        {
+            Console.WriteLine("running");
+        }
+
+     }
+}
